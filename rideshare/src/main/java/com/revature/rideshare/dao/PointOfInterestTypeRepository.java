30a1000a0d7328c9b43da08acaeb5f5d45a13753
Siddharth <sid.desai.n@gmail.com>
2017-08-24 16:47:37 -0400

    New Branch
    updated branch

--- rideshare/src/main/java/com/revature/rideshare/dao/PointOfInterestTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.rideshare.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.rideshare.domain.PointOfInterestType;
-
-public interface PointOfInterestTypeRepository extends JpaRepository<PointOfInterestType, Long> {
-
-}
