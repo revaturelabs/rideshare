acca07e73f97989e6d90f3bda13d7dd50b5b194e
stalindlrp <hansennickolas1@gmail.com>
2017-08-25 10:18:15 -0400

    Util class Made

--- rideshare/src/main/java/com/revature/rideshare/util/EnviromentalVariables.java
+++ rideshare/src/main/java/com/revature/rideshare/util/EnviromentalVariables.java
@@ -2,4 +2,9 @@ package com.revature.rideshare.util;
 
 public class EnviromentalVariables {
 
+	public final static String  JWTSecret = "#{systemEnvironment['RIDESHARE_JWT_SECRET']}";
+	public final static String  slackID = "#{systemEnvironment['RIDESHARE_SLACK_ID']}";
+	public final static String  slackSecret = "#{systemEnvironment['RIDESHARE_SLACK_SECRET']}";
+	public final static String  slackVerify = "#{systemEnvironment['RIDESHARE_SLACK_VERIFICATION']}";
+	public final static String  slackTeam = "#{systemEnvironment['RIDESHARE_SLACK_TEAM']}";
 }
