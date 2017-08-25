911eaa64c23a7a3c98ee4be2a1a9e8ea187a6852
Kyle Bennett <KyleEdwardBennett@gmail.com>
2017-08-25 13:08:47 -0400

    finished basic test of slackMessageService.createPoiSelectDestinationAttachment(String callbackID)

--- rideshare/src/test/java/com/revature/rideshare/service/SlackMessageServiceTests.java
+++ rideshare/src/test/java/com/revature/rideshare/service/SlackMessageServiceTests.java
@@ -1,5 +1,7 @@
 package com.revature.rideshare.service;
 
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -11,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.rideshare.domain.PointOfInterest;
 import com.revature.rideshare.json.Action;
 import com.revature.rideshare.json.Attachment;
 import com.revature.rideshare.json.Option;
@@ -25,7 +28,7 @@ public class SlackMessageServiceTests {
 	RideService rideService;
 
 	@InjectMocks
-	SlackMessageService slackMessageService;
+	SlackMessageService slackMessageService = new SlackMessageServiceImpl();
 
 	public List<Option> getToFromOptions() {
 		List<Option> toFromOptions = new ArrayList<Option>();
@@ -42,25 +45,59 @@ public class SlackMessageServiceTests {
 
 	}
 
+	public List<PointOfInterest> getMockPoiList() {
+		List<PointOfInterest> poiList = new ArrayList<PointOfInterest>();
+		return poiList;
+	}
+
+	List<Option> getPoiOptions()
+	{		
+		List<PointOfInterest> pois = getMockPoiList();
+		List<Option> poiOptions = new ArrayList<Option>();
+		for (PointOfInterest poi : pois) {
+			Option o = new Option(poi.getPoiName(), poi.getPoiName());
+			poiOptions.add(o);
+		}
+		
+		return poiOptions;
+		
+	}
+	
 	@Test
 	public void testCreatePoiSelectDestinationAttachment() {
 
-		String callbackID = "findRidesMessage";
+//		String callbackID = "findRidesMessage";
+		String callbackID = "SomeCallbackID";
+		
+		List<PointOfInterest> poiList = getMockPoiList();
+		
+		when(poiService.getAll()).thenReturn(poiList);
 
 		Attachment testAttachment = slackMessageService.createPoiSelectDestinationAttachment(callbackID);
 
+		//Fails if the attachment's callbackID is not properly set based off input.
+		assert(testAttachment.getCallback_id().equals(callbackID));
+		
 		List<Action> testActions = testAttachment.getActions();
 
+		//Fails if the number of actions in the attachment is not exactly two.
 		assert (testActions.size() == 2);
 
 		List<Option> toFromOptions = getToFromOptions();
 
-		Action toFromAction = new Action("To/From","To/From","select",toFromOptions);
+		Action toFromAction = new Action("To/From", "To/From", "select", toFromOptions);
+
+		List<Option> poiOptions = getPoiOptions();
 		
+		Action poiAction = new Action("POI", "Pick a destination", "select", poiOptions);
+		
+		//Fails if the list does not contain the To/From action;
 		assert (testActions.contains(toFromAction));
 
-//		when(poiService.getAll()).then;
+		//Fails if the list does not contain the Action containing the list of POIs
+		assert (testActions.contains(poiAction));
 		
+		//Fails if slackMessageService.createPoiSelectDestinationAttachment(String) does not query the POI service for points of interest.
+		verify(poiService, atLeastOnce()).getAll();
 	}
-
 }
