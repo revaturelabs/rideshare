b269831187adff548a0ca085cfa2bed4f50d8f6f
Kyle Bennett <KyleEdwardBennett@gmail.com>
2017-08-31 16:28:29 -0400

    All actions required by SlackMessageService are now generated by SlackActionService

--- rideshare/src/main/java/com/revature/rideshare/service/SlackActionServiceRideShare.java
+++ rideshare/src/main/java/com/revature/rideshare/service/SlackActionServiceRideShare.java
@@ -1,6 +1,7 @@
 package com.revature.rideshare.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -68,38 +69,85 @@ public class SlackActionServiceRideShare implements SlackActionService {
 
 	@Override
 	public Action getCreateAvailableRidesAction(List<AvailableRide> availableRides) {
-		// TODO Auto-generated method stub
+		List<Option> options = new ArrayList<Option>();
+		String destinationText = "";
+		String alternateDestinationText = "";
+		for (AvailableRide ride : availableRides) {
+			if (ride.isOpen()) {
+				destinationText = ride.getPickupPOI().getPoiName();
+				alternateDestinationText = ride.getDropoffPOI().getPoiName();
+			}
+			Date time = ride.getTime();
+			String hours = "" + time.getHours();
+			String minutes = "" + time.getMinutes();
+			String meridian = "AM";
+			if (minutes.equals("0")) {
+				minutes = minutes + "0";
+			}
+			if (time.getHours() >= NUMBER_OF_HOURS) {
+				meridian = "PM";
+				if (time.getHours() > NUMBER_OF_HOURS) {
+					hours = "" + (time.getHours() - NUMBER_OF_HOURS);
+				}
+			}
+			String timeText = hours + ":" + minutes + meridian;
+			String text = timeText + " " + destinationText + ">" + alternateDestinationText + " ID:"
+					+ ride.getAvailRideId();
+			Option o = new Option(text, text);
+			options.add(o);
+		}
 		return null;
+
 	}
 
 	@Override
 	public Action getCreateHoursAction() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Option> hourOptions = new ArrayList<Option>();
+		for (int i = 1; i <= NUMBER_OF_HOURS; i++) {
+			Option o = new Option(Integer.toString(i), Integer.toString(i));
+			hourOptions.add(o);
+		}
+		Action hourAction = new Action("Hour", "hour", "select", hourOptions);
+		return hourAction;
 	}
 
 	@Override
 	public Action getCreateMinutesAction() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Option> minuteOptions = new ArrayList<Option>();
+		for (int i = 0; i <= MAX_MINUTES; i = i + MINUTES_INCREMENT) {
+			Option o;
+			if (i == 0)
+				o = new Option(Integer.toString(i) + "0", Integer.toString(i) + "0");
+			else
+				o = new Option(Integer.toString(i), Integer.toString(i));
+			minuteOptions.add(o);
+		}
+		Action minuteAction = new Action("Minute", "minute", "select", minuteOptions);
+		return minuteAction;
 	}
 
 	@Override
 	public Action getCreateMeridianAction() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Option> meridians = new ArrayList<Option>();
+
+		Option am = new Option("AM", "AM");
+		Option pm = new Option("PM", "PM");
+		meridians.add(am);
+		meridians.add(pm);
+
+		Action meridianAction = new Action("Meridian", "AM/PM", "select", meridians);
+
+		return meridianAction;
 	}
 
 	@Override
 	public Action getCreateOKAYAction() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Action("OKAY", "OKAY", "button", "okay");
 	}
 
 	@Override
 	public Action getCreateCancelAction() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Action("cancel", "CANCEL", "button", "cancel");
 	}
 
 }
