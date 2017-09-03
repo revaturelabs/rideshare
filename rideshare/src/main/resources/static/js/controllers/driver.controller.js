878a99ccf8cc38d0682486d2e9f7895aa9128dc2
stalindlrp <hansennickolas1@gmail.com>
2017-09-03 16:42:39 -0400

    Revert "Merge pull request #232 from rideshare1707/Stephanie_Metts"
    This reverts commit 1f4c9b4af47529482d02cc64519f34826c438319, reversing
    changes made to 1b9e619ce4e27d7c273ebd9eaa888bdbb9b37ec0.

--- rideshare/src/main/resources/static/js/controllers/driver.controller.js
+++ rideshare/src/main/resources/static/js/controllers/driver.controller.js
@@ -1,4 +1,4 @@
-export let driverController = function($scope, $http, $state, $cookies){
+export let driverController = function($scope, $http, $state){
 	/*
 	 * Scope and function used to pass ride data to front end
 	 */
@@ -9,7 +9,6 @@ export let driverController = function($scope, $http, $state, $cookies){
 	$scope.openRequest = [];
 	$scope.activeRides = [];
 	$scope.pastRides = [];
-	
 	
 	/*
 	 * Global variables
@@ -24,20 +23,7 @@ export let driverController = function($scope, $http, $state, $cookies){
 	$scope.updateSort = function (item){
 		$http.get("/ride/request/open/"+item.poiId)
 		.then(function(response) {
-			//remove the ignored requests from the response
-			var ignoredRequests =JSON.parse(ignoredRequestsArray);
-			$scope.openRequest = response.data;
-			for(let i = 0; i < $scope.openRequest.length; i++){
-				for(let p=0, p<ignoredRequests.length, p++) {
-					if(response.data[i].requestId == ignoredRequests[p]) {
-						$scope.openRequest.splice(i, 1);
-						console.log(openRequest[i]);
-						$scope.$apply;
-					}
-				}
-			}
-			setTimeout(function(){$state.reload();}, 500);
-			
+			$scope.openRequest = response.data;	
 		});
 	}
 
@@ -273,13 +259,47 @@ export let driverController = function($scope, $http, $state, $cookies){
 	}
 	
 	/*
+	 * Simply print ignore request when function is called
+	 */
+	$scope.ignoreReq = function() {
+		console.log("ignore request test");
+	}
+	
+	//ignore open requests
+/*	$scope.ignoreReq3 = function(id) {
+		//set up this endpoint
+		console.log("Ignore Request Clicked!");
+		$http.get("/ride/request/ignore/"+id)
+		.then(function(response) => {
+			console.log("Ignore Request Response!")
+			$scope.openRequest = response.data;
+			setTimeout(function(){$state.reload();}, 500);
+		});
+	}
+	*/
+	
+	
+	/*
 	 * Ignore requests by calling the ignoreRequest method in RideController.java 
 	 * with the form "/request/ignore/{id}"
 	 */
 	$scope.ignoreReq = function(reqId) {
-		ignoredRequestsArray.put(reqId);
-		$cookie.put('ignoredRequests', JSON.stringify(ignoredRequestsArray));
-		console.log(JSON.stringify(ignoredRequestsArray));
+		
+		$http.get('/ride/request/ignore/' + reqId)
+			.then((response) => {
+				for(let i = 0; i < $scope.openRequest.length; i++){
+					if($scope.openRequest[i].requestId == reqId) {
+						$scope.openRequest.splice(i, 1);
+						console.log(openRequest[i]);
+						$scope.$apply;
+					}
+				}
+				
+				$scope.ignoreReqVar = response.data;
+				$scope.openRequest= response.data;
+				setTimeout(function(){$state.reload();}, 500);
+			}
+		);
 	};
 	
 	$scope.ignoreReqAlert = function(reqId) {
