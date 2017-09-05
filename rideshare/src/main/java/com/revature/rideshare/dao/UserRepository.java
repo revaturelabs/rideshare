7963b514de1fc88bac177c07c30f3be979605c95
Andy Duncan <duncanandyw@gmail.com>
2017-09-05 13:55:55 -0400

    added Javadoc comments to UserRepository

--- rideshare/src/main/java/com/revature/rideshare/dao/UserRepository.java
+++ rideshare/src/main/java/com/revature/rideshare/dao/UserRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.revature.rideshare.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	/**
+	 * Finds a user by their Slack Identification.
+	 * 
+	 * @param slackId the Slack Identification of the user we are searching for
+	 * @return the user we were searching for
+	 * @see com.revature.rideshare.domain.User The User class
+	 */
 	User findBySlackId(String slackId);
 
 }
