5e18ef3981e388dbd99ec091444be5d8acd5a6cf
stalindlrp <hansennickolas1@gmail.com>
2017-09-06 09:53:08 -0400

    Revert "Merge pull request #282 from rideshare1707/Testing"
    This reverts commit baa973675525b47e15dd0156f40ee918f06ddc25, reversing
    changes made to e1ef7068b225f38fa6adc3dd10cb25dff667caa0.

--- rideshare/bin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This is a shell script for executing the entired build process for the Revature RideShare project.
-
-# Install all JavaScript dependencies
-npm install
-# Run webpack
-npm run build
